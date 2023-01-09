3abdc94f877421f0fb473ca630aefe8806b7ff17
alan <alangeorgemathews09@gmail.com>
2023-01-09 13:24:19 +0530

    Added few files for practice

---
+++ comparison.js
@@ -0,0 +1,7 @@
+let result = 5 > 4; // assign the result of the comparison
+console.log(result);
+console.log('Z' > 'A'); 
+console.log('Glow' > 'Glee');
+console.log('Bee' > 'Be'); 
+console.log(null === undefined);
+console.log(null == undefined);
---
+++ strings.js
@@ -0,0 +1,7 @@
+const one = "hi";
+const two = "hello";
+const wee = `${one}${two}`;
+console.log(wee);
+const greeting = "Hello";
+const name = "Chris";
+console.log(greeting + ", " + name);
