549ff4a367fa050fa4e61c7a9faadafcddf33247
alan <alangeorgemathews09@gmail.com>
2023-01-13 13:12:53 +0530

    Added some functions

--- functions/functions.js
+++ functions/functions.js
@@ -2,3 +2,8 @@ const textBox = document.querySelector("#textBox");
 const output = document.querySelector("#output");
 
 textBox.addEventListener('keydown', (event) => output.textContent = `You pressed "${event.key}".`)
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!"
+}
+
+console.log(favoriteAnimal('Goat'))
